6014cf096339d33cc297d55458c7eaddcdf9361a
xiaoba <wen_xiaoba@163.com>
2019-12-24 22:43:59 +0800

    数组拷贝

--- java_les_maven/src/test/java/com/zeroten/javales/array/ArrayTest.java
+++ java_les_maven/src/test/java/com/zeroten/javales/array/ArrayTest.java
@@ -55,4 +55,49 @@ public class ArrayTest {
                 System.out.println(String.format("值为%d，是奇数", val));
         });
     }
+
+    @Test
+    public void testArrayTraversal2(){
+//        三种⽅式遍历数组 Integer[] arr = {1, 2, 3, 4, 5};
+        int[] arr = {1, 2, 3, 4, 5};
+
+//        第1种：for 循环使⽤数组下标
+        for(int index = 0; index < arr.length; index++){
+            System.out.println(String.format("下标%d存放的值是：%d", index, arr[index]));
+        }
+
+//        第2种：for each 循环
+        int index = 0;
+        for(int num: arr){
+            System.out.println(String.format("数组下标%d存放的值是：%d", index, arr[index]));
+            index++;
+        }
+
+//        第3种：Java 8 中新增的 Lambda 表达式，基本数据类型不能使用
+//        因为Arrays.asList(T... a)参数类型是 T ，根据官方文档的描述，T 是数组元素的 class，基本数据类型无class属性
+//        故基本数据类型不能使用Lambda表达式来遍历数组
+//        Arrays.asList(arr).forEach(val -> {
+//            if(val % 2 == 0)
+//                System.out.println(String.format("值为%d，是偶数", val));
+//            else
+//                System.out.println(String.format("值为%d，是奇数", val));
+//        });
+    }
+
+    @Test
+    public void testCopy(){
+        Integer[] arr1 = {1, 2, 3, 4, 5};
+        Integer[] arr2 = {6, 7, 8, 9, 10};
+//      拷⻉数组 arr1 的前 3 个元素
+        Integer[] arr3 = Arrays.copyOf(arr1, 3);    //arr3 = [1, 2, 3]
+        System.out.println(Arrays.toString(arr3));
+
+//      拷⻉数组 arr1 的第 1 位到第 3 位的元素（不包括第 3 位）
+        Integer[] arr4 = Arrays.copyOfRange(arr1, 1, 3);
+//      arr4 = [2, 3]，即arr1的下标1到2的值
+        System.out.println(Arrays.toString(arr4));
+//      拷⻉数组 arr2 的后 3 位，放到 arr1 下标1之后的位置进行填充
+        System.arraycopy(arr2, 2, arr1, 1, 3);  //arr1 = [1, 8, 9, 10, 5]
+        System.out.println(Arrays.toString(arr1));
+    }
 }
